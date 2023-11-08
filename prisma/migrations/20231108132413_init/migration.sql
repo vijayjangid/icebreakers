4a39a3a544ab1ff4c399db2d67e20527829c4c01
Vijay Jangid <vijskt@gmail.com>
2023-11-08 13:38:38 +0000

    revert

--- prisma/migrations/20231108132413_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Todo" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "title" TEXT NOT NULL,
-    "completed" BOOLEAN NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updatedAt" DATETIME NOT NULL
-);
